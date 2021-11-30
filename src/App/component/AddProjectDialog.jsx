b71bda438eb24c67f644e0cee767088aea68e7a2
hellenGUAN <40756699+hellenGUAN@users.noreply.github.com>
2021-11-30 16:44:32 +0800

    Remove Add repo field from project create dialog (#14)
    * addRepositoryDialog radio button
    
    * add radio button
    
    * final
    
    * recover
    
    * 1
    
    * final
    
    * final
    
    * recover
    
    * recover
    
    * new line at the end
    
    Co-authored-by: TU-Lin <trusaidlin@gmail.com>
    
    Co-authored-by: TU-Lin <trusaidlin@gmail.com>

--- src/App/component/AddProjectDialog.jsx
+++ src/App/component/AddProjectDialog.jsx
@@ -1,7 +1,5 @@
 import {useEffect, useState} from 'react'
 import Axios from 'axios'
-import InputAdornment from '@material-ui/core/InputAdornment';
-import {SiGithub, SiSonarqube} from 'react-icons/si'
 
 import {
   Button,
@@ -15,90 +13,49 @@ import {
 
 export default function AddProjectDialog({open, reloadProjects, handleClose}) {
   const [projectName, setProjectName] = useState("")
-  const [githubRepositoryURL, setGithubRepositoryURL] = useState("")
-  const [sonarRepositoryURL, setSonarRepositoryURL] = useState("")
-  const setIsGithubAvailable = useState(false)[1]
-  const setIsSonarAvailable = useState(false)[1]
   const jwtToken = localStorage.getItem("jwtToken")
 
-  const createProject = () => {
-    let checker = []
-    if (projectName === "" || (githubRepositoryURL === "" && sonarRepositoryURL === "")) {
+  const createProject = async () => {
+    if (projectName.trim() === "") {
       alert("不準啦馬的>///<")
-    } else {
-      if (githubRepositoryURL !== "") {
-        checker.push(checkGithubRepositoryURL());
-      }
-      if (sonarRepositoryURL !== "") {
-        checker.push(checkSonarRepositoryURL());
-      }
-
-      Promise.all(checker)
-        .then((response) => {
-          if (response.includes(false) === false) {
-            let payload = {
-              projectName: projectName,
-              githubRepositoryURL: githubRepositoryURL,
-              sonarRepositoryURL: sonarRepositoryURL
-            }
-
-            Axios.post("http://localhost:9100/pvs-api/project", payload,
-              {headers: {"Authorization": `${jwtToken}`}})
-              .then(() => {
-                reloadProjects()
-                handleClose()
-              })
-              .catch((e) => {
-                alert(e.response.status)
-                console.error(e)
-              })
-          }
-        }).catch((e) => {
-        alert(e.response.status)
-        console.error(e)
-      })
+      return
     }
+
+    const payload = {
+      projectName,
+      githubRepositoryURL: "",
+      sonarRepositoryURL: ""
+    }
+
+    const config = {
+      headers: {
+        ...(jwtToken && {"Authorization": jwtToken})
+      }
+    }
+
+    try {
+      await Axios.post("http://localhost:9100/pvs-api/project", payload, config)
+    } catch (e) {
+      alert(e?.response?.status)
+      console.error(e)
+    } // 回傳給後端
+
+    reloadProjects()
+    handleClose()
   }
 
-  const checkGithubRepositoryURL = () => {
-    return Axios.get(`http://localhost:9100/pvs-api/repository/github/check?url=${githubRepositoryURL}`,
-      {headers: {"Authorization": `${jwtToken}`}})
-      .then(() => {
-        setIsGithubAvailable(true);
-        return true
-      })
-      .catch(() => {
-        alert("github error")
-        return false
-      })
-  }
-
-  const checkSonarRepositoryURL = () => {
-    return Axios.get(`http://localhost:9100/pvs-api/repository/sonar/check?url=${sonarRepositoryURL}`,
-      {headers: {"Authorization": `${jwtToken}`}})
-      .then(() => {
-        setIsSonarAvailable(true);
-        return true
-      })
-      .catch((e) => {
-        alert("sonar error")
-        console.error(e)
-        return false
-      })
-  }
-
+  // 刷新
   useEffect(() => {
     setProjectName("")
-    setGithubRepositoryURL("")
-    setSonarRepositoryURL("")
   }, [open])
 
+  // dialog 介面
   return (
     <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
       <DialogTitle id="form-dialog-title">Create Project</DialogTitle>
       <DialogContent>
         <DialogContentText>
-          To create a project, please enter the project name and the repository URL you want to subscribe here.
+          To create a project, please enter the project name.
         </DialogContentText>
         <TextField
           autoFocus
@@ -109,43 +66,6 @@ export default function AddProjectDialog({open, reloadProjects, handleClose}) {
           fullWidth
           onChange={(e) => {
             setProjectName(e.target.value)
-          }}
-        />
-        <TextField
-          margin="dense"
-          id="GithubRepositoryURL"
-          label="Github Repository URL"
-          type="text"
-          fullWidth
-          onChange={(e) => {
-            setGithubRepositoryURL(e.target.value)
-          }}
-          required
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SiGithub/>
-              </InputAdornment>
-            ),
-          }}
-        />
-
-        <TextField
-          margin="dense"
-          id="SonarRepositoryURL"
-          label="Sonar Repository URL"
-          type="text"
-          fullWidth
-          onChange={(e) => {
-            setSonarRepositoryURL(e.target.value)
-          }}
-          required
-          InputProps={{
-            startAdornment: (
-              <InputAdornment position="start">
-                <SiSonarqube/>
-              </InputAdornment>
-            ),
           }}
         />
       </DialogContent>
