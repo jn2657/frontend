bca6d362ca09d35c099c41a8f6688f8f03df8da6
Xanonymous <trusaidlin@gmail.com>
2022-01-05 03:26:03 +0800

    fix: fix fixer scripts

--- deployment_url_fixer.py
+++ deployment_url_fixer.py
@@ -1,17 +1,22 @@
-import fileinput
 from os import walk
 
 
 def start() -> None:
-    target_path: str = './src/App/component'
+    target_paths: [str] = [
+        './src/App/component/',
+        './src/App/component/DashBoardComponent/'
+    ]
+
     replace_from: str = 'http://localhost:9100'
     replace_to: str = ''
 
-    filenames = next(walk(target_path), (None, None, []))[2]
-    for filename in filenames:
-        with fileinput.FileInput(filename, inplace=True) as file:
-            for line in file:
-                line.replace(replace_from, replace_to)
+    for path in target_paths:
+        filenames = next(walk(path), (None, None, []))[2]
+        for filename in filenames:
+            with open(path + filename, 'r') as file:
+                file_data = file.read().replace(replace_from, replace_to)
+                with open(path + filename, 'w') as new_file:
+                    new_file.write(file_data)
 
 
 if __name__ == '__main__':
